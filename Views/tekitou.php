47138dedf76cbbdf09051a8e775c96bfbb9db51d
nakasonekanta <135171910+nakasonekanta@users.noreply.github.com>
2023-07-17 15:01:48 +0900

    ホーム画面PHPの組み込み

---
+++ Views/tekitou.php
@@ -0,0 +1,433 @@
+
+<!DOCTYPE html>
+<html lang="ja" itemscope itemtype="http://schema.org/WebPage">
+  <head>
+    <meta charset="UTF-8">
+    
+    <title>田舎高校</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="http://www.miyako-h.open.ed.jp/styles.css">
+    <!--[if lt IE 9]>
+    <link rel="stylesheet" href="http://www.miyako-h.open.ed.jp/styles_ie.css">
+    <script src="/mt-static/support/theme_static/eiger/js/html5shiv.js"></script>
+    <![endif]-->
+    
+    <link rel="start" href="http://www.miyako-h.open.ed.jp/">
+    <link rel="alternate" type="application/atom+xml" title="Recent Entries" href="http://www.miyako-h.open.ed.jp/atom.xml">
+    <link rel="canonical" href="http://www.miyako-h.open.ed.jp/" />
+
+    <!-- Open Graph Protocol -->
+    <meta property="og:type" content="article">
+    <meta property="og:locale" content="ja_JP">
+    <meta property="og:title" content="沖縄県立宮古高等学校">
+    <meta property="og:url" content="http://www.miyako-h.open.ed.jp/">
+    
+    <meta property="og:site_name" content="沖縄県立宮古高等学校">
+    <meta property="og:image" content="http://www.miyako-h.open.ed.jp/mt-static/support/theme_static/eiger/images/siteicon-sample.png">
+    <!-- Microdata -->
+    <meta itemprop="description" content="">
+    <link itemprop="url" href="http://www.miyako-h.open.ed.jp/">
+    <link itemprop="image" href="http://www.miyako-h.open.ed.jp/mt-static/support/theme_static/eiger/images/siteicon-sample.png">
+  </head>
+  <body>
+    <header role="banner">
+      <h1 class="title">
+    <a href="http://www.miyako-h.open.ed.jp/">
+
+      沖縄県立宮古高等学校
+
+    </a>
+  </h1>
+
+      <nav role="navigation">
+          <ul>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/blog/">ブログ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/post.html">学校長あいさつ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/entrance-exam.html">高校入試情報</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/schedule.html">年間行事予定</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/2021adelante.html">【随時更新】進路だより（アデランテ）</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/kesseki-tikoku-system.html">欠席・遅刻連絡システム </a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/typhoon-info.html">暴風（台風）時の登校について</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/coronavirus-influenza.html">保健室より（コロナ・インフル関連）</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/jimu-shinsei.html">各種証明書の申請手続きについて</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/contact.html">お問い合わせ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/youran.html">学校要覧</a></li>
+
+          </ul>
+        </nav>
+
+    </header>
+    <div id="mainimage">
+
+      <img alt="沖縄県立宮古高等学校" src="http://www.miyako-h.open.ed.jp/assets_c/2015/09/Top-thumb-800x300-8835.jpg">
+
+    </div>
+
+    <div class="content">
+      <div role="main">
+        <section id="posts">
+          <h2>最近の記事</h2>
+          <ol>
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/07/post-95.html">
+                <time datetime="2023-07-07T10:58:15+09:00">2023年7月 7日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">事務室より</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/05/2-2.html">
+                <time datetime="2023-05-31T11:11:06+09:00">2023年5月31日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">台風2号に伴う登校について</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/05/post-94.html">
+                <time datetime="2023-05-26T13:38:44+09:00">2023年5月26日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和５年度教育実習生の皆様へ</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/05/post-93.html">
+                <time datetime="2023-05-26T10:29:40+09:00">2023年5月26日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和５年度学校要覧･シラバス(授業計画)を更新しました</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/05/post-92.html">
+                <time datetime="2023-05-11T09:01:37+09:00">2023年5月11日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和５年度第１回宮古高校授業参観･PTA総会について</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/04/5-4.html">
+                <time datetime="2023-04-27T13:29:39+09:00">2023年4月27日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和5年度宮古地区高校総体について</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/04/post-91.html">
+                <time datetime="2023-04-19T13:01:44+09:00">2023年4月19日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">卒業生の皆様へ(進路より)</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/03/post-90.html">
+                <time datetime="2023-03-30T08:30:48+09:00">2023年3月30日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">新入生(合格者)の皆様へ</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/03/5-3.html">
+                <time datetime="2023-03-15T08:58:31+09:00">2023年3月15日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和5年度一般入試合格発表について</span>
+                </div>
+              </a>
+            </li>
+  
+
+  
+            <li>
+              <a href="http://www.miyako-h.open.ed.jp/blog/2023/03/5-2.html">
+                <time datetime="2023-03-14T16:26:19+09:00">2023年3月14日</time>
+                <div>
+  
+    
+      
+                  <span class="label category information">お知らせ</span>
+      
+    
+  
+                  <span class="title">令和5年度一般入試合格発表について</span>
+                </div>
+              </a>
+            </li>
+  
+          </ol>
+          <nav>
+            <a href="http://www.miyako-h.open.ed.jp/blog/">記事一覧へ</a>
+          </nav>
+        </section>
+  
+
+
+      </div>
+      <div class="related">
+        <nav class="widget-search widget">
+  <form method="get" id="search" action="http://www.miyako-h.open.ed.jp/cgi-bin/movabletype/mt/mt-search.cgi">
+    <div>
+      <input type="text" name="search" value="" placeholder="検索...">
+
+      <input type="hidden" name="IncludeBlogs" value="111">
+
+      <input type="hidden" name="limit" value="20">
+      <button type="submit" name="button">
+        <img alt="検索" src="/mt-static/support/theme_static/eiger/images/search-icon.png">
+      </button>
+    </div>
+  </form>
+</nav>
+<nav class="widget-pages widget">
+  <h3>ウェブページ</h3>
+  <ul>
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/2021adelante.html">【随時更新】進路だより（アデランテ）</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/ijime-kihonhoushin.html">いじめ防止基本方針</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/syllabus.html">シラバス</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/post-2.html">スクールポリシー</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/post-3.html">事務室より</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/coronavirus-influenza.html">保健室より（コロナ・インフル関連）</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/jimu-shinsei.html">各種証明書の申請手続きについて</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/bylaws.html">学校内規</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/http:/www.miyako-h.open.ed.jp/5f71d6514e6a642d42f33c679029fe7cc27022e2.pdf.html">学校案内パンフレット</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/gakkou-hyouka.html">学校評価</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/post.html">学校長あいさつ</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/schedule.html">年間行事予定</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/typhoon-info.html">暴風（台風）時の登校について</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/kousho-kouka.html">校章・校訓・校歌</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/kesseki-tikoku-system.html">欠席・遅刻連絡システム </a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/saigai-shiryou.html">災害共済給付の手続きについて</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/http:/www.miyako-h.open.ed.jp/2fcf316a985e92cc388bc0375e5b088de2a3f660.pdf.html">生徒支援細則(改訂版)</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/shihikaikei.html">私費会計取扱マニュアル（改訂版）</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/bukatsudou-katsudouhoushin.html">部活動に係わる活動方針</a></li>
+      
+    
+      
+    <li><a href="http://www.miyako-h.open.ed.jp/entrance-exam.html">高校入試情報</a></li>
+      
+  </ul>
+</nav>
+      
+    
+  
+
+<div class="widget">
+  <h3>リンク</h3>
+
+  <a href="https://www.office.com/">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/jpeg/office365.jpg">
+  </a>
+  <a href="https://www.pref.okinawa.lg.jp/edu/shien/shugakushien/shienmenu.html">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/79033f791aa0faad13a781bab7d1a1ebefe41047.JPG">
+  </a>
+  <a href="https://www.mext.go.jp/a_menu/ikusei/gakusyushien/index_00001.htm">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/items/event/coronapage_20200313.jpg">
+  </a>
+  <a href="https://www.mext.go.jp/a_menu/coronavirus/index.html">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/jpeg/mext.jpg">
+  </a>
+  <a href="http://www.syugaku.open.ed.jp/index.html">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/jpeg/tyutaipc.jpg">
+  </a>
+  <a href="http://www.syugaku.open.ed.jp/mobile/index.html">
+    <img alt="" src="http://www.miyako-h.open.ed.jp/jpeg/tyutaisma.jpg">
+  </a>
+</div>
+
+      </div>
+    </div>
+    <footer role="contentinfo">
+      <nav role="navigation">
+          <ul>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/blog/">ブログ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/post.html">学校長あいさつ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/entrance-exam.html">高校入試情報</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/schedule.html">年間行事予定</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/2021adelante.html">【随時更新】進路だより（アデランテ）</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/kesseki-tikoku-system.html">欠席・遅刻連絡システム </a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/typhoon-info.html">暴風（台風）時の登校について</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/coronavirus-influenza.html">保健室より（コロナ・インフル関連）</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/jimu-shinsei.html">各種証明書の申請手続きについて</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/contact.html">お問い合わせ</a></li>
+
+            <li><a href="http://www.miyako-h.open.ed.jp/youran.html">学校要覧</a></li>
+
+          </ul>
+        </nav>
+
+<p class="license">&copy; Copyright 沖縄県立宮古高等学校.All Rights Reserve</p>
+
+    </footer>
+    <script src="http://www.miyako-h.open.ed.jp/mt-static/jquery/jquery.min.js"></script>
+    <script src="http://www.miyako-h.open.ed.jp/mt-theme-eiger.js"></script>
+  </body>
+</html>
