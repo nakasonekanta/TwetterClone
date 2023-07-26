6ecc5d9a532e2e6c461d59e970a7506b82974ee8
nakasonekanta <135171910+nakasonekanta@users.noreply.github.com>
2023-07-26 14:48:18 +0900

    ログイン機能

--- Views/home.php
+++ Views/home.php
@@ -1,29 +1,4 @@
-<?php
-//設定関連を読み込む
-include_once('../config.php');
-//便利な関数を読み込む
-include_once('../util.php');
 
-///////////////////////////////////////
-// ツイート一覧
-///////////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-03-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-   
-]; 
- 
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
  
